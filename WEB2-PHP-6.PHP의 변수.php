606afd23370ec755e5c8adde2016160979e030c6
izonejangwonyoung <2000shim@naver.com>
2020-06-27 02:48:39 +0900

    $name 변수를 활용한 간단 작업법

--- WEB2-PHP-6.PHP의 변수.php
+++ WEB2-PHP-6.PHP의 변수.php
@@ -5,11 +5,16 @@
   <body>
       <h1>variable</h1>
       <?php
-      $a=1;
+      $name="장원영";
+       echo "Lorem ipsum ".$name." dolor sit amet, consectetur adipisicing elit,
+       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+        enim ad minim veniam, quis nostrud exercitation ul".$name."lamco laboris nisi
+        ut aliquip ex ".$name."ea commodo consequat. Duis aute irure dolor in reprehender
+        it in voluptate velit esse cillum dolore eu fugiat null".$name."a pariatur. Excepte
+        ur sint occaecat cupidatat non proident, sunt i".$name."n culpa qui officia deserunt
+         mollit anim id es".$name."t laborum. by ".$name;
 
-      echo $a+1;
-       ?>
-
+        ?>
 
   </body>
 </html>
