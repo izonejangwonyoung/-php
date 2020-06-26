503d786f844cf909a32c27ab5468f7dd5ef32eb1
izonejangwonyoung <2000shim@naver.com>
2020-06-27 04:07:03 +0900

    드디어 수정 성공;; 1시간 걸림

--- WEB2-PHP-7.2.URL 파라미터의 활용.php
+++ WEB2-PHP-7.2.URL 파라미터의 활용.php
@@ -7,19 +7,24 @@
   <body>
     <h1>IZONE</h1>
     <ol>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영">장원영</li>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원">김채원</li>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미">혼다 히토미</li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영.txt">장원영</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원.txt">김채원</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미.txt">혼다 히토미</a></li>
     </ol>
 
     <h2>
     <?php
-    echo $_GET['id'];
+    echo $_GET["id"];
 
 
 
      ?>
-    </h2>
-     사랑스러운 귀염둥이들
+    <h2>
+
+       <?php
+
+       echo file_get_contents("data/".$_GET["id"]);
+        ?>
+        </h2>
   </body>
 </html>
--- WEB2-PHP-8.2.함수의 활용.php
+++ WEB2-PHP-8.2.함수의 활용.php
@@ -7,18 +7,18 @@
   <body>
     <h1>IZONE</h1>
     <ol>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영">장원영</a></li>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원">김채원</a></li>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미">혼다 히토미</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영">장원영.txt</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원">김채원.txt</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미">혼다 히토미.txt</a></li>
     </ol>
-
     <h2>
-    <?php
-    echo $_GET['id'];
+      <?php
+        echo $_GET['id'];
+    ?>
+    <h2>
+     <?php
 
-
-
-     ?>
-
+     echo file_get_contents("data/".$_GET['id']);
+      ?>
   </body>
 </html>
