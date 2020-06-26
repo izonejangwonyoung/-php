408586936429f073b4dd554aaba43367d1b47922
izonejangwonyoung <2000shim@naver.com>
2020-06-27 03:41:06 +0900

    /./

---
+++ WEB2-PHP-8.2.함수의 활용.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>IZONE</h1>
+    <ol>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영">장원영</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원">김채원</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미">혼다 히토미</a></li>
+    </ol>
+
+    <h2>
+    <?php
+    echo $_GET['id'];
+
+
+
+     ?>
+
+  </body>
+</html>
