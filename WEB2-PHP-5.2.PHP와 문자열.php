23596a52f802a2767d88964b5a68c1d9d2decd6c
izonejangwonyoung <2000shim@naver.com>
2020-06-27 02:36:29 +0900

    ..

--- WEB2-PHP-5.2.PHP와 문자열.php
+++ WEB2-PHP-5.2.PHP와 문자열.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>string % string operation</h1>
+    <?php
+    echo "hello \"w\"orld";
+    ?>
+     <h2>concatenation operator</h2>
+     <?php
 
-echo "hello "w"orld";
+     echo "hello"."world";
 
- ?>
+
+
+      ?>
+  </body>
+</html>
