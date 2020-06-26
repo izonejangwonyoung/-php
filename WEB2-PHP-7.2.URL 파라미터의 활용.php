bf09a5780bc7075cdce4b43ef2639b853eb9e16f
izonejangwonyoung <2000shim@naver.com>
2020-06-27 03:11:09 +0900

    완성

---
+++ WEB2-PHP-7.2.URL 파라미터의 활용.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>IZONE</h1>
+    <ol>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영">장원영</li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원">김채원</li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미">혼다 히토미</li>
+    </ol>
+
+    <h2>
+    <?php
+    echo $_GET['id'];
+
+
+
+     ?>
+    </h2>
+     사랑스러운 귀염둥이들
+  </body>
+</html>
