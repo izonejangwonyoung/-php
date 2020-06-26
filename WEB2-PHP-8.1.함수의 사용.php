5eccd75f29d2c7415fe7f0186a7dd882b7fdf030
izonejangwonyoung <2000shim@naver.com>
2020-06-27 03:20:40 +0900

    nl2br

---
+++ WEB2-PHP-8.1.함수의 사용.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <h1>function</h1>
+      <?php
+      $str="하나가 되는 순간.
+      모두가 주목해!";
+      echo $str;
+
+
+      ?>
+
+
+      <?php
+    echo  strlen($str)
+
+
+       ?>
+<h2>nl2br</h2>
+
+  <?php
+    echo nl2br($str);
+
+    ?>
+
+
+
+
+  </body>
+</html>
