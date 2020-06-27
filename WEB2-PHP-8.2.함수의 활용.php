8bba87afdba9ffb3f52da93030aaa9596a2f29b7
izonejangwonyoung <2000shim@naver.com>
2020-06-28 01:21:27 +0900

    ..

--- WEB2-PHP-8.2.함수의 활용.php
+++ WEB2-PHP-8.2.함수의 활용.php
@@ -9,7 +9,8 @@
     <ol>
       <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영">장원영.txt</a></li>
       <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원">김채원.txt</a></li>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미">혼다 히토미.txt</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미">혼다 히토미.txt
+      </a></li>
     </ol>
     <h2>
       <?php
