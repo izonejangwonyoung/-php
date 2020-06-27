11489e935c75678e7989c84ba7ee71afb53c3050
izonejangwonyoung <2000shim@naver.com>
2020-06-28 01:51:35 +0900

    ..

---
+++ WEB2-PHP-11.Boolean과 비교 연산자.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>comparison operators &amp;
+    Boolean data type</h1>
+    <?php
+    var_dump('11');
+
+     ?>
+     <br><br>  <br><br>// 문자열이고 2개의 문자로 이루어져있다는 것을 알려줌.
+       //  실서버에서는 쓸 일 없음
+<h2>1==1</h2>
+<?php
+    var_dump(1==1);
+?>
+
+<h2>1>1</h2>
+<?php
+var_dump(1>1);
+ ?>
+<h2>1>=1</h2>
+<?php
+var_dump(1>=1);
+ ?>
+
+
+
+
+  </body>
+</html>
