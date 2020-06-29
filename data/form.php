bc046f9e80f1297ff4e18f298b7f54bca02c9bab
izonejangwonyoung <2000shim@naver.com>
2020-06-29 21:40:13 +0900

    '

--- data/form.php
+++ data/form.php
@@ -1,5 +1,5 @@
 <?php
-
-echo "<p>title: ".$_GET['title']."</p>";
-echo "<p>description: ".$_GET['description']."</p>";
+file_put_contents('data/'.$_GET['title'],$_GET['description']);
+// echo "<p>title: ".$_GET['title']."</p>";
+// echo "<p>description: ".$_GET['description']."</p>";
 ?>
