2e87b1c502b44724ce49578c756cae2aee079bdf
izonejangwonyoung <2000shim@naver.com>
2020-06-29 20:17:41 +0900

    ..

--- WEB2-PHP-7.2.URL 파라미터의 활용.php
+++ WEB2-PHP-7.2.URL 파라미터의 활용.php
@@ -7,17 +7,35 @@
   <body>
     <h1><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php">IZONE</a></h1>
     <ol>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영.txt">장원영</a></li>
+      <?php
+      $list=scandir('./data');
+
+      $i=0;
+      while($i<count($list)){
+        echo "<li><a href=\"WEB2-PHP-7.2.URL 파라미터의 활용.php?id=$list[$i]\">$list[$i]</a></li>\n";
+        $i=$i+1;
+
+      }
+
+
+      // echo "<li>$list[0]</li>\n";
+      // echo "<li>$list[1]</li>\n";
+      // echo "<li>$list[2]</li>\n";
+      // echo "<li>$list[3]</li>\n";
+      // echo "<li>$list[4]</li>\n";
+      // echo "<li>$list[5]</li>\n";
+
+
+
+       ?>
+
+
+      <!-- <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=장원영.txt">장원영</a></li>
       <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=김채원.txt">김채원</a></li>
       <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=혼다 히토미.txt">혼다 히토미</a></li>
-      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=아이즈원.txt">아이즈원</a></li>
+      <li><a href="WEB2-PHP-7.2.URL 파라미터의 활용.php?id=아이즈원.txt">아이즈원</a></li> -->
 
     </ol>
-<?php
-$list=scandir('./data');
-var_dump($list);
- ?>
-
 
     <h2>
     <?php
