2b7fb4d6e670f442b352a984aa68fb969df239c2
izonejangwonyoung <2000shim@naver.com>
2020-06-29 18:19:15 +0900

    ..

---
+++ WEB2-PHP-15.반복문의 형식.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Loop</title>
+  </head>
+  <body>
+    <h1>while</h1>
+
+    <?php
+
+echo "1<br>";
+$i=0;
+while ($i<20) {
+echo "2<br>";
+$i=$i+1;
+
+}
+
+
+
+
+echo "3<br>";
+     ?>
+  </body>
+</html>
