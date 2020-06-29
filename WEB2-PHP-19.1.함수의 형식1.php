743b36b189fd30c6d2f506f0c396ad815e91ba52
izonejangwonyoung <2000shim@naver.com>
2020-06-29 20:56:30 +0900

    ..

---
+++ WEB2-PHP-19.1.함수의 형식1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>fuction</title>
+  </head>
+  <body>
+    <h1>Fucntion</h1>
+    <h2>Basic</h2>
+<?php
+  function basic(){
+    print("1");
+    print("2");
+
+}
+
+      basic();
+      basic();
+ ?>
+
+
+  </body>
+</html>
---
+++ WEB2-PHP-19.1.함수의 형식2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>fuction</title>
+  </head>
+  <body>
+    <h1>Fucntion</h1>
+    <h2>Basic</h2>
+<?php
+  function basic(){
+    print("1<br>");
+    print("2<br>");
+
+}
+
+      basic();
+      basic();
+ ?>
+ <h2>parameter &amp; argument</h2>
+<?php
+function sum($left,$right){
+
+print($left+$right);
+print("<br>");
+
+  }
+  sum(54895457464,456876786544);
+  sum(4,-5.5)
+        ?>
+ <
+
+
+
+
+
+
+
+  </body>
+</html>
