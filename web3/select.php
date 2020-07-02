afaffaf5d08dc36b9971f25db73c257f4e3b7406
izonejangwonyoung <2000shim@naver.com>
2020-07-03 00:50:11 +0900

    SELECT 사용법 3

--- web3/select.php
+++ web3/select.php
@@ -5,12 +5,26 @@ $conn=mysqli_connect(
 'wh001500',
 'izone');
 
+
 $sql = " select * from izone_information  where member_count=6";
 $result=mysqli_query($conn,$sql);
+
+
+echo "<h1>single row<h1>";
 $row=mysqli_fetch_array($result);
 echo '<h1>'.$row['name'].'</h1>';
 echo $row['profile'];
 
+echo "<h1>multi row<h1>";
+$row=mysqli_fetch_array($result);
+echo '<h1>'.$row['name'].'</h1>';
+echo $row['profile'];
+
+
+echo "<h1>triple row<h1>";
+$row=mysqli_fetch_array($result);
+echo '<h1>'.$row['name'].'</h1>';
+echo $row['profile'];
 
 
 
