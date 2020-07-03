f01fd1aacd5a29b8ad18686db4e2b22710330ada
izonejangwonyoung <2000shim@naver.com>
2020-07-03 20:59:10 +0900

    ..

---
+++ web2-python/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<?php
+echo "아이즈원 사랑해";
+
+<a href="helloworld.py"></a>
+ ?>
+
+  </body>
+</html>
