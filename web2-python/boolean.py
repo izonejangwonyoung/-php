4b7ceefd084b71598a55fffbbb670c1e92212f7f
izonejangwonyoung <2000shim@naver.com>
2020-07-05 01:02:29 +0900

    ..

--- web2-python/boolean.py
+++ web2-python/boolean.py
@@ -1,6 +1,20 @@
 #number
 
 print(1)
+
+print('113')
+print(15*468)
+
+
+#string
+
+print('hello,\nworld')
+
+#boolean
+print(True)
+print(False)
+# True=1;
+
+#expression
 print(1+2)
-print('1')
-print(1)
+print('hello'+'\nworld')
