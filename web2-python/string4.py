c06c549d347b508e91c9115b86bdc095a2d049b0
izonejangwonyoung <2000shim@naver.com>
2020-07-04 16:27:45 +0900

    named placeholder

--- web2-python/string4.py
+++ web2-python/string4.py
@@ -4,3 +4,9 @@ print('To {} ,{} by {}'.format('izone','test','izone'))
 
 
 
+#named placeholder 중복 사라지고 어떤 취지의 data가 오는지 알 수 있어서 가독성 향상
+
+
+print('To {name} ,{test} by {name}'.format(name='izone',test='export test'))
+
+
