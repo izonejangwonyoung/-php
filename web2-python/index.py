4d75f277e4efe71914eac2422665d7a6bace414d
izonejangwonyoung <2000shim@naver.com>
2020-07-04 21:28:31 +0900

    ..

---
+++ web2-python/index.py
@@ -0,0 +1,30 @@
+#!C:\Users\ericshim\AppData\Local\Programs\Python\Python38\python.exe
+print("Content-Type: text/html")
+print()
+import cgi
+form = cgi.fieldstorage()
+pageId = form.getvalue("id")
+print(pageId)
+print('''<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>IZONE</title>
+  </head>
+  <body>
+    <h1><a href="index.html">IZONE</a></h1>
+    <ol>
+      <li><a href="index.py?id=count_1">count_1</a></li>
+      <li><a href="index.py?id=count_2">count_2</a></li>
+      <li><a href="index.py?id=count_3">count_3</a></li>
+         </ol>
+<h2>{title}</h2>
+<p>test</p>
+  </body>
+</html>
+
+
+
+
+'''.format(title='pageId')
+)
