a2d6d3a63e089844581e466c18c1071976309d2e
izonejangwonyoung <2000shim@naver.com>
2020-07-05 11:16:32 +0900

    if문

--- web2-python/conditional.py
+++ web2-python/conditional.py
@@ -1,2 +1,15 @@
-if xxx:
-    yyy
+id_user=input("ID:")
+pwd_user=input("PWD:")
+
+
+'''if password_user == "1111":
+    print("응 아니야")
+else:
+    print("ㅇㅋ")'''
+
+
+if id_user=='2000shim':
+    if pwd_user == "1111":
+        print("hello admin")
+else:
+    print("no data in DB. please sign up.")
