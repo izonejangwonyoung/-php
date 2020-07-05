e8bf9c594fce4fa0f493f7f0d06b5e57c5b8a402
izonejangwonyoung <2000shim@naver.com>
2020-07-05 11:26:30 +0900

    and 연산자

--- web2-python/conditional.py
+++ web2-python/conditional.py
@@ -2,14 +2,26 @@ id_user=input("ID:")
 pwd_user=input("PWD:")
 
 
-'''if password_user == "1111":
+'''
+if password_user == "1111":
     print("응 아니야")
 else:
-    print("ㅇㅋ")'''
+    print("ㅇㅋ")
 
+'''
+
+'''
 
 if id_user=='2000shim':
     if pwd_user == "1111":
         print("hello admin")
 else:
     print("no data in DB. please sign up.")
+    
+'''
+
+if id_user == '2000shim' and pwd_user == "1111":
+    print("hello admin")
+else:
+    print("who are you?")
+
