701b8596803408be414491cdab4634071b501960
izonejangwonyoung <2000shim@naver.com>
2020-07-05 13:38:08 +0900

    list dictionary type

---
+++ web2-python/containers.py
@@ -0,0 +1,9 @@
+print("a"+"b")
+print(['a']+['b'])
+print('izone'.capitalize())
+
+izone_list=['김채원','장원영','안유진','나코']
+print(izone_list[0])
+
+member={'name':'장원영','성별':'여성'}
+print(member['name'])
