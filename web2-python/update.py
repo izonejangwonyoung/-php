6b2e0cc1f4448f5bc867141b78f508cccd6d176f
izonejangwonyoung <2000shim@naver.com>
2020-07-05 19:32:59 +0900

    강의 틀려서 스스로 수정

--- web2-python/update.py
+++ web2-python/update.py
@@ -31,8 +31,9 @@ print('''<!DOCTYPE html>
 {listStr}
     </ol>
 
-<form action  ="process_create.py" method="post">
+<form action  ="process_update.py" method="post">
 
+<input type="hidden" name="pageId" value= "{form_default_title}">
 <a href="create.py">create</a>
     <p><input type="text" name="title" placeholder="title" value= "{form_default_title}"></p>
     <p><textarea rows="5" name="description" placeholder="description">{form_default_description}</textarea></p>
