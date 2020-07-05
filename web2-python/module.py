40e7a7372f54cab03ae7f7b5ee5fe5914cf24b82
izonejangwonyoung <2000shim@naver.com>
2020-07-05 20:40:16 +0900

    module practice2 without import text

--- web2-python/module.py
+++ web2-python/module.py
@@ -1,5 +1,16 @@
+'''
 import math2
 
 
 print(math2.average(1,2,3))
 print(math2.plus(1,2))
+
+'''
+
+
+from math2 import average
+from math2 import plus
+
+
+print(average(1,2,3))
+print(plus(1,2))
